cfc65fad35240da8f1ffe1edf6cc3d0f2f6f7c12
RohanDey02 <58871401+RohanDey02@users.noreply.github.com>
2021-12-21 13:15:42 -0500

    Implemented Contact page

---
+++ src/components/ContactInfoItem.js
@@ -0,0 +1,45 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import styled from 'styled-components';
+import { MdEmail } from 'react-icons/md';
+import PText from './PText';
+
+const ItemStyles = styled.div`
+    padding: 2rem;
+    background-color: var(--deep-dark);
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    border-radius: 8px;
+    margin-bottom: 2rem;
+    .icon {
+        color: var(--white);
+        background-color: var(--gray-2);
+        padding: 1.3rem;
+        align-items: center;
+        justify-content: center;
+        border-radius: 50%;
+    }
+    svg {
+        width: 3.5rem;
+    }
+
+`
+
+export default function ContactInfoItem({
+    icon = <MdEmail />,
+    text = 'Default contact info',
+}) {
+    return (
+        <ItemStyles>
+            <div className='icon'>
+                {icon}
+            </div>
+            <div className='info'>
+                <PText>
+                    {text}
+                </PText>
+            </div>
+        </ItemStyles>
+    );
+}
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,11 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
+import ContactSection from '../components/ContactSection';
 
 export default function Contact() {
 	return (
 		<div>
-			<h1>Hello</h1>
+			<ContactSection />
 		</div>
 	);
 }
