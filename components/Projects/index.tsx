2e8223a485ffd23d20836b975c44ceb6459c1917
Rohan Dey <58871401+RohanDey02@users.noreply.github.com>
2024-04-29 13:07:15 -0400

    Added projects

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import projectData from "./projects.json";
-import ExperienceEntry from "../../types/ExperienceEntry";
+import ProjectEntry from "../../types/ProjectEntry";
 
-const ProjectEntryCard: React.FC<ExperienceEntry> = ({ startYear, endYear, role, company, companyUrl, description, technologies }) => {
+const ProjectEntryCard: React.FC<ProjectEntry> = ({ year, project, projectUrl, description, technologies }) => {
     return (            
         <div className="exp-card glass-hover">
             <div className="exp-card-left text-xs uppercase tracking-wide text-slate-500 sm:col-span-2">
-                <p>{startYear} - {endYear}</p>
+                <p>{year}</p>
             </div>
             <div className="exp-card-right">
-                <a className="hover:text-teal-300" href={companyUrl}>
-                    <h3>{role} · {company}</h3>
+                <a className="hover:text-teal-300" href={projectUrl}>
+                    <h3>{project}</h3>
                 </a>
                 <p className="text-slate-400 mt-2 text-sm leading-normal">{description}</p>
                 <ul className="mt-2 flex flex-wrap" aria-label="technologies">
@@ -29,7 +29,7 @@ const ProjectEntryCard: React.FC<ExperienceEntry> = ({ startYear, endYear, role,
 
 const Projects = () => {
     return <div className="experience-list">
-        {projectData.map((experience: ExperienceEntry, index: number) => (
+        {projectData.map((experience: ProjectEntry, index: number) => (
             <ProjectEntryCard key={index} {...experience} />
         ))}
     </div>;
